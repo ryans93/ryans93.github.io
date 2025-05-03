66f886eeee31bb37dc1218738bd66fe8e6e452c5
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro.local>
2025-05-03 18:13:23 -0400

    adjusted project layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -21,25 +21,27 @@ function Project(props) {
             <h3 className="w-[100%] flex flex-row justify-center text-white text-4xl font-bold border-white border-b-3 pb-[1rem] mb-[1rem]">Blood Sugar App</h3>
             <section className="flex flex-col">
                 <div className="basis-[100%]  border-white border-b-3  pb-[1rem] ">
-                    <img src={content.image} className="w-full h-full max-h-[50vh] object-contain"/>
+                    <img src={content.image} className="w-full h-full max-h-[50vh] object-contain" />
                 </div>
-                <div className="basis-[100%] border-b-3 pr-[1rem] my-[1rem] ">
-                    <h3 className="w-[100%] flex flex-row justify-center text-white text-2xl font-bold mb-[1rem]">Description</h3>
-                    <p className="pl-[1rem]"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{content.description}</p>
-                    <div className="text-l font-semibold  w-full flex justify-evenly py-[1rem]">
-                        <a href={content.deployedLink} target="__blank" className="hover:underline hover:cursor-pointer"><span>Live Demo</span></a>
-                        <a href={content.githubLink} target="__blank" className="hover:underline hover:cursor-pointer"><span>Git Repo</span></a>
+                <div className="flex flex-col md:flex-row">
+                    <div className="basis-[100%] border-b-3 md:border-r-3 md:border-b-0 pr-[1rem] my-[1rem] ">
+                        <h3 className="w-[100%] flex flex-row justify-center text-white text-2xl font-bold mb-[1rem]">Description</h3>
+                        <p className="pl-[1rem]"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{content.description}</p>
+                        <div className="text-l font-semibold  w-full flex justify-evenly py-[1rem]">
+                            <a href={content.deployedLink} target="__blank" className="hover:underline hover:cursor-pointer"><span>Live Demo</span></a>
+                            <a href={content.githubLink} target="__blank" className="hover:underline hover:cursor-pointer"><span>Git Repo</span></a>
+                        </div>
                     </div>
-                </div>
-                <div className="basis-[100%]">
-                <h3 className="w-[100%] flex flex-row justify-center text-white text-2xl font-bold mb-[1rem] pl-[1rem]">Technologies Used</h3>
-                    <ul className="list-disc list-inside pl-[1rem] flex flex-col text-center ">
-                        {content.techs.map(tech => {
-                            return (
-                                <li key={tech}>{tech}</li>
-                            )
-                        })}
-                    </ul>
+                    <div className="basis-[100%] md:my-[1rem]">
+                        <h3 className="w-[100%] flex flex-row justify-center text-white text-2xl font-bold mb-[1rem] pl-[1rem]">Technologies Used</h3>
+                        <ul className="list-disc list-inside pl-[1rem] flex flex-col text-center ">
+                            {content.techs.map(tech => {
+                                return (
+                                    <li key={tech}>{tech}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </section>
         </div>
