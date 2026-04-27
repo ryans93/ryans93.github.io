b27644f1a3419c2d8a58bd7703223895b99735ad
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro.local>
2026-04-26 21:22:31 -0400

    fixed header display for smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import lnIcon from "../assets/icons/icons8-linkedin-ios-17-glyph/icons8-linkedin
 
 function Header() {
     return (
-        <section id="header" className="bg-red-700/50 text-white flex w-[85%] grow-0 flex md:flex-row flex-col h-fit p-[2rem] my-[3rem] border-white-500 border-3">
+        <section id="header" className="bg-red-700/50 text-white flex w-[85%] grow-0 flex md:flex-row flex-col h-fit lg:p-[2rem] p-[1rem] my-[3rem] border-white-500 border-3">
             <div className="basis-1/3 flex justify-center items-center md:border-r md:border-r-2 md:border-r-white-500 md:border-b-0 border-b-2 md:border-b-white-500 py-[1rem]" >
                 <img src="Ryan.Strickler_7410_square.jpg" alt="Headshot pic" width="80%" height="80%" className="rounded-full" />
             </div>
-            <div className="basis-2/3 flex flex-col justify-evenly items-center">
+            <div className="basis-2/3 flex flex-col justify-evenly items-center lg:pl-[2rem] pl-[1rem]">
                 <h1 className="text-4xl font-extrabold py-[1rem]">Ryan Strickler</h1>
                 <h4 className="text-2xl font-bold">QA / Software Testing | Debugging & Full-Stack Systems</h4>
-                <h5 className="flex w-full lg:flex-row lg:justify-evenly flex-col items-center py-[1rem] underline">
+                <h5 className="flex w-full xl:flex-row xl:justify-evenly flex-col items-center py-[1rem] underline">
                     <a className="text-xl font-bold " href="mailto:ryan.strickler.1993@gmail.com">Email: ryan.strickler.1993@gmail.com</a>
                     <a className="text-xl font-bold " href="tel:+13524595396">Phone: (352) 459-5396</a>
                 </h5>
